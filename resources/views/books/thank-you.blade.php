da589770033417325e0a42340ccd55147ce073bf
isidrei <musni.danisidrei@auf.edu.ph>
2022-04-22 16:45:49 +0800

    filled the missing codes

--- resources/views/books/thank-you.blade.php
+++ resources/views/books/thank-you.blade.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -16,6 +17,14 @@
 
     <ul class="list-unstyled">
     <!-- 1. Display all the reserved books -->
+
+        @foreach ($reserved_books as $book)
+        <li>
+            <strong>{{ $book['title'] }}</strong> by {{ $book['author'] }}, {{ $book['year_published'] }}
+        </li>
+        @endforeach
+
+
     </ul>
 
 </div>
